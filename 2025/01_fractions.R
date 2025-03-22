339cffad0112f63743c8ff48f53471abc49d48b1
greg-dubrow <dubrowg@gmail.com>
2025-03-22 19:10:30 +0100

    prompt 1 2025

--- 2025/01_fractions.R
+++ 2025/01_fractions.R
@@ -87,7 +87,7 @@ edattain %>%
 			 x = "", y = "Age Group") +
 	theme_minimal() +
 	theme(legend.position = "bottom", legend.spacing.x = unit(0, 'cm'),
-				legend.key.width = unit(2.5, 'cm'), legend.margin=margin(-10, 0, 0, 0),
+				legend.key.width = unit(4, 'cm'), legend.margin=margin(-10, 0, 0, 0),
 				legend.text = element_text(size = 10), legend.title = element_text(size = 12),
 				plot.title = element_text(hjust = .5, size = 18),
 				plot.subtitle = element_text(size = 16),
